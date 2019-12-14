7d176117f434132f3bb2ec0c3a2fcee67700c291
Ashwin <ashanz93@outlook.com>
2019-12-14 11:15:47 +0530

    Patch Validation and Upsert, Delete

--- Controllers/AuthorsController.cs
+++ Controllers/AuthorsController.cs
@@ -64,5 +64,20 @@ namespace CourseLibrary.API.Controllers
             Response.Headers.Add("Allow", "GET, OPTIONS, POST");
             return Ok();
         }
+
+        [HttpDelete("{authorId}")]
+        public ActionResult DeleteAuthor(Guid authorId)
+        {
+            var authorFromRepo = _courseLibraryRepository.GetAuthor(authorId);
+
+            if (authorFromRepo == null)
+            {
+                return NotFound();
+            }
+
+            _courseLibraryRepository.DeleteAuthor(authorFromRepo);
+            _courseLibraryRepository.Save();
+            return NoContent();
+        }
     }
 }
--- Controllers/CoursesController.cs
+++ Controllers/CoursesController.cs
@@ -3,6 +3,10 @@ using CourseLibrary.API.Entities;
 using CourseLibrary.API.Services;
 using Microsoft.AspNetCore.JsonPatch;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Infrastructure;
+using Microsoft.AspNetCore.Mvc.ModelBinding;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Options;
 using RESTAPITutorial.Models;
 using System;
 using System.Collections.Generic;
@@ -117,15 +121,59 @@ namespace RESTAPITutorial.Controllers
 
             if (courseFromAuthorRepo == null)
             {
-                return NotFound();
+                var courseDto = new CourseForUpdateDto();
+                patchDocument.ApplyTo(courseDto, ModelState);
+                if (!TryValidateModel(courseDto))
+                {
+                    return ValidationProblem(ModelState);
+                }
+                var courseToAdd = _mapper.Map<Course>(courseDto);
+                courseToAdd.Id = courseId;
+
+                _courseLibraryRepository.AddCourse(authorId, courseToAdd);
+                _courseLibraryRepository.Save();
+
+                var courseToReturn = _mapper.Map<CourseDto>(courseToAdd);
+                return CreatedAtRoute("GetCourseForAuthor", new { authorId, courseId = courseToReturn.Id }, courseToReturn);
             }
 
             var courseToPatch = _mapper.Map<CourseForUpdateDto>(courseFromAuthorRepo);
-            patchDocument.ApplyTo(courseToPatch);
+            patchDocument.ApplyTo(courseToPatch, ModelState);
+            // PATCH validation (only handles existing resource paths and not non -existent ones. hence we need to override. Retrun InvalidModelStateResponseFactory)
+            if(!TryValidateModel(courseToPatch))
+            {
+                return ValidationProblem(ModelState);
+            }
             _mapper.Map(courseToPatch, courseFromAuthorRepo);
             _courseLibraryRepository.UpdateCourse(courseFromAuthorRepo);
             _courseLibraryRepository.Save();
             return NoContent();
+        }
+
+        [HttpDelete("{courseId}")]
+        public ActionResult DeleteCourseForAuthor(Guid authorId, Guid courseId)
+        {
+            if (!_courseLibraryRepository.AuthorExists(authorId))
+            {
+                return NotFound();
+            }
+
+            var courseFromAuthorRepo = _courseLibraryRepository.GetCourse(authorId, courseId);
+
+            if(courseFromAuthorRepo == null)
+            {
+                return NotFound();
+            }
+
+            _courseLibraryRepository.DeleteCourse(courseFromAuthorRepo);
+            _courseLibraryRepository.Save();
+            return NoContent();
+        }
+
+        public override ActionResult ValidationProblem([ActionResultObjectValue] ModelStateDictionary modelStateDictionary)
+        {
+            var options = HttpContext.RequestServices.GetRequiredService<IOptions<ApiBehaviorOptions>>();
+            return (ActionResult)options.Value.InvalidModelStateResponseFactory(ControllerContext);
         }
     }
 }
